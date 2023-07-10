d46fd569b272f6032c6bb9576434bc7f3a83efa5
Minh giang trương <minhgiang241@gmail.com>
2023-07-11 00:54:09 +0700

    add a basic project

---
+++ WPFTutorial/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFTutorial.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set { placeholder = value; }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            tbPlaceholder.Visibility = Visibility.Hidden;
+        }
+        private void txtInput_Changed(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text)) tbPlaceholder.Visibility = Visibility.Visible;
+        }
+    }
+}
